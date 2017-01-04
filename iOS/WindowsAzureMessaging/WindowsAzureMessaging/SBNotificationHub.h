4a2cf99d89fb9da6f95109ba53219391a49468bb
mnespor <mnespor@fnoex.com>
2017-01-04 17:38:29 -0600

    add type hints to SBNotificationHub.h

--- iOS/WindowsAzureMessaging/WindowsAzureMessaging/SBNotificationHub.h
+++ iOS/WindowsAzureMessaging/WindowsAzureMessaging/SBNotificationHub.h
@@ -22,10 +22,10 @@
 - (SBNotificationHub*) initWithConnectionString:(NSString*) connectionString notificationHubPath:(NSString*)notificationHubPath;
 
 // Async operations
-- (void) registerNativeWithDeviceToken:(NSData*)deviceToken tags:(NSSet*)tags completion:(void (^)(NSError* error))completion;
-- (void) registerTemplateWithDeviceToken:(NSData*)deviceToken name:(NSString*)name jsonBodyTemplate:(NSString*)bodyTemplate expiryTemplate:(NSString*)expiryTemplate tags:(NSSet*)tags completion:(void (^)(NSError* error))completion;
+- (void) registerNativeWithDeviceToken:(NSData*)deviceToken tags:(NSSet<NSString*>*)tags completion:(void (^)(NSError* error))completion;
+- (void) registerTemplateWithDeviceToken:(NSData*)deviceToken name:(NSString*)name jsonBodyTemplate:(NSString*)bodyTemplate expiryTemplate:(NSString*)expiryTemplate tags:(NSSet<NSString*>*)tags completion:(void (^)(NSError* error))completion;
 
-- (void) retrieveAllRegistrationsWithDeviceTokenData:(NSData*)deviceTokenData completion:(void (^)(NSArray*, NSError*))completion;
+- (void) retrieveAllRegistrationsWithDeviceTokenData:(NSData*)deviceTokenData completion:(void (^)(NSArray<SBRegistration*>*, NSError*))completion;
 
 - (void) unregisterNativeWithCompletion:(void (^)(NSError* error))completion;
 - (void) unregisterTemplateWithName:(NSString*)name completion:(void (^)(NSError* error))completion;
@@ -33,10 +33,10 @@
 - (void) unregisterAllWithDeviceToken:(NSData*)deviceToken completion:(void (^)(NSError* error))completion;
 
 // sync operations
-- (BOOL) registerNativeWithDeviceToken:(NSData*)deviceToken tags:(NSSet*)tags error:(NSError**)error;
-- (BOOL) registerTemplateWithDeviceToken:(NSData*)deviceToken name:(NSString*)templateName jsonBodyTemplate:(NSString*)bodyTemplate expiryTemplate:(NSString*)expiryTemplate tags:(NSSet*)tags error:(NSError**)error;
+- (BOOL) registerNativeWithDeviceToken:(NSData*)deviceToken tags:(NSSet<NSString*>*)tags error:(NSError**)error;
+- (BOOL) registerTemplateWithDeviceToken:(NSData*)deviceToken name:(NSString*)templateName jsonBodyTemplate:(NSString*)bodyTemplate expiryTemplate:(NSString*)expiryTemplate tags:(NSSet<NSString*>*)tags error:(NSError**)error;
 
-- (NSArray*) retrieveAllRegistrationsWithDeviceTokenData:(NSData*)deviceTokenData error:(NSError**)error;
+- (NSArray<SBRegistration*>*) retrieveAllRegistrationsWithDeviceTokenData:(NSData*)deviceTokenData error:(NSError**)error;
 
 - (BOOL) unregisterNativeWithError:(NSError**)error;
 - (BOOL) unregisterTemplateWithName:(NSString*)name error:(NSError**)error;
